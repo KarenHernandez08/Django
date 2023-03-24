431c02f61e9efabb97b87f49e5ad30b4e92c0845
Karen Jocelyn <ada_hack109@hackademy.mx>
2023-03-24 13:29:07 -0600

    cambios

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super (PublishedManager,
+                      self).get_queryset()\
+                           .filter(status='published')
+    
 class Post (models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -17,6 +23,9 @@ class Post (models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models. DateTimeField(auto_now = True)
     status = models.CharField(max_length=10, choices= STATUS_CHOICES, default='draft')
+    
+    objects= models.Manager()
+    published=PublishedManager()
 
 
     class Meta: 
@@ -25,9 +34,7 @@ class Post (models.Model):
     def __str__(self) :
         return self.title
     
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super (PublishedManager,
-                      self).get_queryset()\
-                           .filter(status='published')
+    def get_absolute_url(self):
+        return reversed('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'),self.publish.strftime('%d'), self.slug])
+
     
